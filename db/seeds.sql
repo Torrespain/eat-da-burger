017ae462b29d1a368915d914cf987e8926df4c65
torrespain <javirorre86@gmail.com>
2018-03-03 19:45:11 -0800

    created orm, controller, model & handlebars

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,2 +1,3 @@
 INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
-INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('The Double Triple Bossy Deluxe');
+INSERT INTO burgers (burger_name) VALUES ('Krusty Burger');
